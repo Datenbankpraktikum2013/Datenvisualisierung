7139504fe65e14b026742ec6c4d50f58526b3a4b
Dominik <mail@dlips.de>
2013-07-30 12:57:42 +0200

    Improves the UI Prototyp

--- public/js/app/columnchart.js
+++ public/js/app/columnchart.js
@@ -2,4 +2,24 @@ var App = App || {};
 
 App.columnchart = {
 
+	render : function() {
+    	$('#chart').highcharts({
+	        chart: {
+	            type: 'bar'
+	        },
+	        title: {
+	            text: 'Fruit Consumption'
+	        },
+	        xAxis: {
+	            categories: ['Apples', 'Bananas', 'Oranges']
+	        },
+	        yAxis: {
+	            title: {
+	                text: 'Fruit eaten'
+	            }
+	        },
+	        series : App.model.data
+    	});
+    }
+
 };
--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,5 @@
 (function($) {
-    var _state;
-    var chart_type = 'bar';
-    var chart;
         
-    $.cookie.json = true;
-    if ($.cookie('formstate')) {
-    	$('form :input:visible').formstate($.cookie('formstate'));
-    }
-    
-    $('button[name="store"]').click(function() {
-        _state = $('form').formstate(':visible');
-        $.cookie('formstate', _state);
-        return false;
-    });
-    
-    $('button[name="restore"]').click(function() {
-        $('form :input:visible').formstate($.cookie('formstate'));
-        return false;
-    });
-    
-    $('button[name="barchart"]').click(function() {
-        chart_type = 'bar';
-        renderChart();
-        return false;
-    });
-    
-    $('button[name="piechart"]').click(function() {
-        chart_type = 'pie';
-        renderChart();
-        return false;
-    });
-    
-    function renderChart() {
-    	$('#chart').highcharts({
-	        chart: {
-	            type: chart_type
-	        },
-	        title: {
-	            text: 'Fruit Consumption'
-	        },
-	        xAxis: {
-	            categories: ['Apples', 'Bananas', 'Oranges']
-	        },
-	        yAxis: {
-	            title: {
-	                text: 'Fruit eaten'
-	            }
-	        },
-	        series : App.model.fetch('hi')
-    	});
-    };
-    renderChart();   
+    App.init();   
     
 })(jQuery);
