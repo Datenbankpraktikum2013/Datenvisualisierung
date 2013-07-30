49e1504583ac1d8c453f880af0b56d44648f2bfa
swenzel2 <swenzel@uos.de>
2013-07-30 12:08:08 +0200

    Changed migration script

--- app/helpers/migration.rb
+++ app/helpers/migration.rb
@@ -4,9 +4,12 @@ def main
 	client = Mysql2::Client.new(:host => "mysql5.serv.uni-osnabrueck",:username => "sosruntime", :password => "soSRuntime", :database => "misdb")
 
 	#Get all studying Students
-	students = client.query("SELECT DISTINCT `STG_MATRIKELNR` AS 'matrNr', `STG_GEBJAHR` AS 'gebJahr', `STG_GESCHLECHT` AS 'geschlecht', `STG_STAATSANGH` AS 'herkStaat'
+	students = client.query("SELECT DISTINCT `STG_MATRIKELNR` AS 'MatrNr', `STG_GEBJAHR` AS 'GebJahr', `STG_GESCHLECHT` AS 'Geschlecht', `STG_STAATSANGH` AS 'Staatsangeh'
 FROM `FKT_STUDIENGAENGE`")
 	#Get study fields for Students
 	studentMap = Hash.new
 	students.each do |student|
-		fieldIDs = client.query("SELECT DISTINCT STG_FACH FROM FKT_STUDIENGAENGE WHERE ")
+		fieldIDs = client.query("SELECT DISTINCT STG_FACH FROM FKT_STUDIENGAENGE WHERE STG_MATRIKELNR = ${}")
+		studentMap[student] = fieldIDs
+		studentDB = Student.new
+		studentDB.
