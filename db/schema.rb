5be6766cd05db86d8cb9f126b2e69c1bc280a903
Christoph <cknauft@uos.de>
2013-08-02 08:02:41 +0200

    .

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20130731112414) do
 
   create_table "countries", force: true do |t|
