46a378f8893ccb4b1323205e52c909773bf72d59
nhellwig <nhellwig@cip10.informatik.uni-osnabrueck.de>
2013-08-05 14:07:03 +0200

    renames iso code in country

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130805111753) do
+ActiveRecord::Schema.define(version: 20130805120457) do
 
   create_table "countries", force: true do |t|
     t.string   "country_name"
