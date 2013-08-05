27f4676b6a0c22ad78e97469a05cf43b3604f18c
Dominik <mail@dlips.de>
2013-08-05 19:34:12 +0200

    Adds FusionTablesLayer to GoogleMaps. Canada still missing.

--- public/js/app/googlemaps.js
+++ public/js/app/googlemaps.js
@@ -7,7 +7,7 @@
 
  App.chart.googlemaps = {
 
- 	if(App.model.data_gmaps.country_iso_code == 'DE'){
+ 	/*if(App.model.data_gmaps.country_iso_code == 'DE'){
  		setmarker : function(latitude, longitude, title){
  		var marker = new google.maps.Marker({		
  			position: new google.maps.LatLng(App.model.data_gmaps.federal_states.cities.latitude,App.model.data_gmaps.federal_states.cities.longitude),
@@ -22,20 +22,39 @@
  			title: "Anzahl der Menschen: " + App.model.data_gmaps.number
  		});
  		marker.setMap(map);
- 	},
+ 	},*/
+
+  addColorLayer : function () {
+    layer = new google.maps.FusionTablesLayer({
+      map: map,
+      heatmap: { enabled: false },
+      query: {
+        select: "col24",
+        from: "18VIAMr_N2Q8_mx6c8NYCgueJ-EvlmPcmr07h2TA",
+        where: ""
+      },
+      options: {
+        styleId: 2,
+        templateId: 2
+      }
+    });
+  },
 
  	render : function(){
-        map = new google.maps.Map(document.getElementById('chart'), {
-          zoom: 6,
-          center: new google.maps.LatLng(52, 10),
-          streetViewControl: false,
-          panControl: false,
-          scaleControl: false,
-          mapTypeControl: false,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
-       
- 		this.setmarker(52,10,"charttetewasdsad");
+    map = new google.maps.Map(document.getElementById('chart'), {
+      maxZoom : 14,
+      minZoom : 4,
+      zoom: 6,
+      center: new google.maps.LatLng(52, 10),
+      streetViewControl: false,
+      panControl: false,
+      scaleControl: false,
+      mapTypeControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    this.addColorLayer();  
+
+ 		//this.setmarker(52,10,"charttetewasdsad");
         
         //this.setmarker(52,10,"test");
 
