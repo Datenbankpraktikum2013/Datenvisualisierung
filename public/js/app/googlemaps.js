eb111a0384c7afada507cf7bd943b9afd8fede1d
tlangemeyer <tlangemeyer@cip03.informatik.uni-osnabrueck.de>
2013-08-06 15:49:25 +0200

    anzahl der marker in googlemaps angepasst

--- public/js/app/googlemaps.js
+++ public/js/app/googlemaps.js
@@ -54,26 +54,31 @@
     });
     this.addColorLayer();
 
-    
-
-   // this.setmarker(App.model.data_gmaps[0].latitude,App.model.data_gmaps[0].longitude, 'title', App.model.data_gmaps[0].number);
     for(var i = 0; i < App.model.data_gmaps.length; i++){
       if(App.model.data_gmaps[i].country_iso_code == 'DE'){
           /* Marker für Deutschland, Zentral gelesen und absolute Anzhal.
            * Auskommentiert weil aktuell nicht gebraucht
            */ 
+        //for(var v = 0; v < App.model.data_gmaps[i].number; v++){
         //this.setmarker(App.model.data_gmaps[i].latitude,App.model.data_gmaps[i].longitude, App.model.data_gmaps[i].country_iso_code, App.model.data_gmaps[i].number)
+        // }
         for(var j = 0; j < App.model.data_gmaps[i].federal_states.length; j++){
           /* Marker für Bundesland, Zentral gelesen und absolute Anzhal.
            * Auskommentiert weil aktuell nicht gebraucht
-           */ 
+           */
+          //for(var c = 0; c < App.model.data_gmaps[i].federal_states[j].number; c++) {
           //this.setmarker(App.model.data_gmaps[i].federal_states[j].latitude,App.model.data_gmaps[i].federal_states[j].longitude,App.model.data_gmaps[i].federal_states[j].federal_states_iso_code, App.model.data_gmaps[i].federal_states[j].number);
+          //}
           for(var n = 0; n < App.model.data_gmaps[i].federal_states[j].cities.length; n++){
-            this.setmarker(App.model.data_gmaps[i].federal_states[j].cities[n].latitude,App.model.data_gmaps[i].federal_states[j].cities[n].longitude,App.model.data_gmaps[i].federal_states[j].cities[n].location_name,App.model.data_gmaps[i].federal_states[j].cities[n].number);
+            for(var b = 0; b < App.model.data_gmaps[i].federal_states[j].cities[n].number; b++){
+              this.setmarker(App.model.data_gmaps[i].federal_states[j].cities[n].latitude,App.model.data_gmaps[i].federal_states[j].cities[n].longitude,App.model.data_gmaps[i].federal_states[j].cities[n].location_name,App.model.data_gmaps[i].federal_states[j].cities[n].number);
+            }
           }
         }
       }else{
-        this.setmarker(App.model.data_gmaps[i].latitude,App.model.data_gmaps[i].longitude, App.model.data_gmaps[i].country_iso_code, App.model.data_gmaps[i].number);
+        for(var m = 0; m < App.model.data_gmaps[i].number; m++){
+          this.setmarker(App.model.data_gmaps[i].latitude,App.model.data_gmaps[i].longitude, App.model.data_gmaps[i].country_iso_code, App.model.data_gmaps[i].number);
+        }
       }
     }
 
