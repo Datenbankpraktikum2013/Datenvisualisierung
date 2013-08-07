a4db77ab8297117140312206e2505716afb0502c
nhellwig <nhellwig@cip10.informatik.uni-osnabrueck.de>
2013-08-07 13:39:00 +0200

    Finally adds GENERIC joinsgit add .git add .git add .

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,11 @@
 class Location < ActiveRecord::Base
 
+	belongs_to :country
 	belongs_to :federal_state
 
-	belongs_to :country
-
 	has_many :students
+
+	scope :with_countries, -> { joins(:country) }
+	scope :with_federal_states, -> { joins(:federal_state) }
+
 end
