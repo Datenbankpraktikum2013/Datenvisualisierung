60d3f0504348af191e5e0ae9b9fafe752e865c74
purtpelhcs <christian@c-benz.de>
2013-08-08 14:32:54 +0200

    Adds Secret Globe files

---
+++ public/js/app/googleglobe.js
@@ -0,0 +1,44 @@
+/*
+ *	Fasst alle Funktionalitaeten zusammen fuer die Anzeige der Google-Maps
+ * gebraucht werden
+ */
+
+ var App = App || {};
+
+ App.chart.googleglobe = {
+
+  
+ 	render : function(){
+    if(!Detector.webgl){
+      Detector.addGetWebGLMessage();
+    } else {
+
+      var container = document.getElementById('container');
+      var globe = new DAT.Globe(container);
+      console.log(globe);
+      var i, tweens = [];
+
+      var xhr;
+      TWEEN.start();
+
+      xhr = new XMLHttpRequest();
+      xhr.open('GET', '/js/globe/students.json', true);
+      xhr.onreadystatechange = function(e) {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+            window.data = data;
+            for (i=0;i<data.length;i++) {
+              globe.addData(data[i][1], {format: 'magnitude', name: data[i][0], animated: true});
+            }
+            globe.createPoints();
+            settime(globe,0)();
+            globe.animate();
+          }
+        }
+      };
+      xhr.send(null);
+    }
+ 	},    
+
+ }
